11bc4f90a5d979e2abc721f2241f20164ea10213
Andrii H <ahrohodza1@gmail.com>
2020-09-15 19:01:41 +0300

    Added close links, set date

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -4,9 +4,33 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
-
+let data = document.getElementById('date');
+data.innerHTML = new Date().getFullYear();
 // ********** close links ************
+let navToggle = document.querySelector('.nav-toggle');
+let linksContainer = document.querySelector('.links-container');
+let links = document.querySelector('.links');
+let containerHeight;
 
+navToggle.addEventListener('click', function(e) {
+	setContainerHeight();
+});
+//get container height
+function getLinksHeight() {
+	linksHeight = links.getBoundingClientRect().height;
+	return linksHeight;
+}
+//set container height
+function setContainerHeight() {
+	let height = getLinksHeight();
+	let containerHeight = linksContainer.getBoundingClientRect().height;
+
+	if (containerHeight === 0) {
+		linksContainer.style.height = `${height}px`;
+	} else {
+		linksContainer.style.height = 0;
+	}
+}
 // ********** fixed navbar ************
 
 // ********** smooth scroll ************
